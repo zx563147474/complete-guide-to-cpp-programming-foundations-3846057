4b221a4a27a382d67a6434f7f0f5b2481313a980
Eduardo Corpeño <kuashio@gmail.com>
2024-07-06 20:23:35 +0000

    reorganized folder structure

--- src/Ch00/00_03/CodeDemo.cpp
+++ src/Ch00/00_03/CodeDemo.cpp
@@ -1,6 +1,6 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 00_03
-// Using the Exercise Files on GitHub Codespaces, by Eduardo Corpeño 
+// Using the Exercise Files in GitHub Codespaces, by Eduardo Corpeño 
 
 #include <iostream>
 
