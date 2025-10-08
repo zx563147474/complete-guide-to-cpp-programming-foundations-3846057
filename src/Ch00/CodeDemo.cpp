c781002de3b74b9cb6befbdee9ab52a70719899d
zx563147474 <zx563147474@gmail.com>
2025-10-08 01:17:12 +0000

    add comment

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -1,5 +1,5 @@
 // Complete Guide to C++ Programming Foundations
-// Exercise 00_03 dsa test
+// Exercise 00_03 dsa test1
 // Using the Exercise Files in GitHub Codespaces, by Eduardo Corpeño 
 
 #include <iostream>
