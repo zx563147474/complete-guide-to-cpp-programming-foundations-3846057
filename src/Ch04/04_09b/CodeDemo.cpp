bde9a7fdaa3f0ae2619a1fc86784372411f477ee
zx563147474 <zx563147474@gmail.com>
2025-10-11 23:05:53 +0000

    practice

--- src/Ch04/04_09b/CodeDemo.cpp
+++ src/Ch04/04_09b/CodeDemo.cpp
@@ -7,13 +7,21 @@
 
 int main(){
     const size_t LENGTH = 50;
-    char racer1[];
+    char racer1[] = "Speedy";
     char racer2[LENGTH];
     char raceResult[LENGTH * 2];
 
+    strncpy(racer2, "Lightning", sizeof(racer2)-1);
+    racer2[sizeof(racer2)-1] = '\0';
     std::cout << "Racer 1: " << racer1 << std::endl;
     std::cout << "Racer 2: " << racer2 << std::endl;
 
+    strncpy(raceResult, "And the winner is ", sizeof(raceResult) - 1);
+    raceResult[sizeof(raceResult) - 1] = '\0';
+    strncat(raceResult, racer1, sizeof(raceResult) - strlen(raceResult) - 1);
+
+    std::cout << raceResult << std::endl;
+    
     std::cout << std::endl << std::endl;
     return 0;
 }
--- src/Ch04/04_10b/CodeDemo.cpp
+++ src/Ch04/04_10b/CodeDemo.cpp
@@ -3,24 +3,17 @@
 // The String Class, by Eduardo Corpeño 
 
 #include <iostream>
-#include <cstring>
+#include <string>
 
 int main(){
-    const size_t LENGTH = 50;
-    char racer1[] = "Speedy";
-    char racer2[LENGTH];
-    char raceResult[LENGTH * 2];
-
-    strncpy(racer2, "Lightning", sizeof(racer2) - 1);
-    racer2[sizeof(racer2) - 1] = '\0';
+    std::string racer1 = "Speedy";
+    std::string racer2 = "Lightning";
+    std::string raceResult;
 
     std::cout << "Racer 1: " << racer1 << std::endl;
     std::cout << "Racer 2: " << racer2 << std::endl;
 
-    strncpy(raceResult, "And the winner is ", sizeof(raceResult) - 1);
-    raceResult[sizeof(raceResult) - 1] = '\0';
-
-    strncat(raceResult, racer1, sizeof(raceResult) - strlen(raceResult) - 1);
+    raceResult = "And the winner is "+ racer1;
 
     std::cout << raceResult << std::endl;
 
