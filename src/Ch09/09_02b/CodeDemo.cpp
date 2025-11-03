1698f314b11a8a6629320da8e01d3894cb2abff5
zx563147474 <zx563147474@gmail.com>
2025-11-03 23:31:37 +0000

    practice

--- src/Ch09/09_02b/CodeDemo.cpp
+++ src/Ch09/09_02b/CodeDemo.cpp
@@ -18,7 +18,13 @@ int main(){
     else{
         while (!inFile.eof()){
             getline(inFile, str);
-            std::cout << str << std::endl;
+            std::cout << str << ", ";
+            getline(inFile, str);
+            number = stoi(str);
+            std::cout << number << ", ";
+            getline(inFile, str);
+            letter = str[0];
+            std::cout << letter << std::endl;
         }
         inFile.close();
     }
